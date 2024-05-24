24f4c35ce9163ce8b4e307e1e86af1df26fc90e6
Alain JUAN <ajuan@flextrade.com>
2024-05-24 08:52:19 +0200

    use https

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   base: '/mottai-iframe-bridge-sample/',
+  server: { https: true }, // Not needed for Vite 5+
 })
